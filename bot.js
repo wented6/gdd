b4f7f593669cf94d3a2c62c6b1b364a4432c3cfc
wented6 <44713416+wented6@users.noreply.github.com>
2019-02-09 22:03:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,33 @@
-﻿
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const developers = ['412547940654252053'];
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setStatus("dnd")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('Mpl')) {
+client.user.setActivity(argresult, {type:'Playing'});
+    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+} else 
+if (message.content.startsWith('Mst')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/meercy");
+   message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+} else
+if (message.content.startsWith('Mls')) {
+   client.user.setActivity(argresult, {type:'Listening'});
+    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+} else
+if (message.content.startsWith('Mwt')) {
+   client.user.setActivity(argresult, {type:'Watching'});
+    message.channel.send(` <: :white_check_mark:  **${argresult} \** `)
+}
+});
+
+client.login(process.env.B);
